9603a1c7b880e85a2dd02f455a8dff32080c9197
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-19 12:28:51 +0900

    Added controllers and paths

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-2.times do
-  Restaurant.create!(name: 'R1', address: 'abc street', category: 'chinese')
+5.times do
+  Restaurant.create!(name: Faker::Company.name, address: Faker::Address.street_name, category: Restaurant::CATEGORIES.sample)
 end
 
-Restaurant.all.each do |restaurant|
-  2.times do
-    Review.create!(content: 'blabla', rating: 4, restaurant_id: restaurant)
-  end
-end
+# Restaurant.all.each do |restaurant|
+#   2.times do
+#     Review.create!(content: 'blabla', rating: 4, restaurant_id: restaurant)
+#   end
+# end
