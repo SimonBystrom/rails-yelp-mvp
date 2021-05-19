7a5efda5f501588778192d4c72492cb5b6e35856
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-19 11:59:06 +0900

    added validations to model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+2.times do
+  Restaurant.create!(name: 'R1', address: 'abc street', category: 'chinese')
+end
+
+Restaurant.all.each do |restaurant|
+  2.times do
+    Review.create!(content: 'blabla', rating: 4, restaurant_id: restaurant)
+  end
+end
